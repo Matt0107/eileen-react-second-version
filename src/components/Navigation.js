a589b41f7582d791298b1f0af4544101ba9d8903
Matt0107 <mathieu.durimel@gmail.com>
2023-02-15 17:40:59 +0100

    media query ajoutées, reste fonction burger menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -28,5 +28,30 @@ const Navigation = () => {
     </div>
   );
 };
+// const burgerMenu = document.querySelector(".burger-menu");
+// const burgerMenuContent = document.querySelector(".burger-menu-content");
+// const closeButton = document.querySelector(".close-button");
+
+// burgerMenu.addEventListener("click", function () {
+//   document.body.classList.toggle("burger-menu-open");
+// });
+
+// closeButton.addEventListener("click", function () {
+//   document.body.classList.remove("burger-menu-open");
+// });
+
+// document.addEventListener("click", function (event) {
+//   if (
+//     !event.target.closest(".burger-menu") &&
+//     !event.target.closest(".burger-menu-content")
+//   ) {
+//     document.body.classList.remove("burger-menu-open");
+//   }
+// });
+// const title = document.querySelector(".title");
+
+// title.addEventListener("click", function () {
+//   window.location.href = "/";
+// });
 
 export default Navigation;
